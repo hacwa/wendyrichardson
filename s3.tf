ad27ef2035f87c287195facc2338470aeb4a4766
Stuart <Stuart.kennedy@ans.co.uk>
2023-04-27 16:59:46 +0100

    works

--- s3.tf
+++ s3.tf
@@ -10,26 +10,32 @@ resource "aws_s3_bucket_ownership_controls" "www_controls" {
 resource "aws_s3_bucket_public_access_block" "s3-pa-block" {
   bucket = "${var.www_domain_name}"
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-resource "aws_s3_account_public_access_block" "s3-account-public-access-block" {
-  block_public_acls   = false
-  block_public_policy = false
-}
-
-resource "aws_s3_bucket_acl" "s3bucket_acl" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.www_controls,
-    aws_s3_bucket_public_access_block.s3-pa-block,
-  ]
+resource "aws_s3_bucket_policy" "mybucketterraformpolicy" {
   bucket = "${var.www_domain_name}"
-  acl    = "public-read"
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
 
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.www.arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
 resource "aws_s3_bucket_website_configuration" "s3-bucket-website-config" {
   bucket = "${var.www_domain_name}"
   index_document {
